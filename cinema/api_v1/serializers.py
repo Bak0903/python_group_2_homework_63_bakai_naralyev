070ac2a83569010d9df2928f2fed03b796dde297
bak0903 <bak0903@mail.ru>
2019-03-04 13:00:24 +0600

    Смигрировал новые модели

--- cinema/api_v1/serializers.py
+++ cinema/api_v1/serializers.py
@@ -1,4 +1,4 @@
-from webapp.models import Movie
+from webapp.models import Movie, Category, Hall, Seat, Show
 from rest_framework import serializers
 
 
@@ -6,3 +6,27 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ('id', 'name', 'description', 'poster', 'release_date', 'finish_date')
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'description')
+
+
+class HallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hall
+        fields = ('id', 'name')
+
+
+class SeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seat
+        fields = ('id', 'hall', 'row', 'place')
+
+
+class ShowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Show
+        fields = ('id', 'film', 'hall', 'start', 'finish', 'price')
--- cinema/webapp/admin.py
+++ cinema/webapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from webapp.models import Movie
+from webapp.models import Movie, Category, Hall, Seat, Show
 
 
 class MovieAdmin(admin.ModelAdmin):
@@ -8,3 +8,7 @@ class MovieAdmin(admin.ModelAdmin):
     search_fields = ['name', 'id']
 
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Category)
+admin.site.register(Hall)
+admin.site.register(Seat)
+admin.site.register(Show)
