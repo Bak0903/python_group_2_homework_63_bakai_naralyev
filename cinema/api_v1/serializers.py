a542bb9ae7d8eb2fa5ed9f907917ba432dde60a4
bak0903 <bak0903@mail.ru>
2019-03-06 14:47:41 +0600

    Добавил сериалайзеры к новым моделям

--- cinema/api_v1/serializers.py
+++ cinema/api_v1/serializers.py
@@ -1,32 +1,99 @@
-from webapp.models import Movie, Category, Hall, Seat, Show
+from webapp.models import Movie, Category, Hall, Seat, Show, Discount, Ticket, Reservation
 from rest_framework import serializers
 
 
-class MovieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Movie
-        fields = ('id', 'name', 'genr','description', 'poster', 'release_date', 'finish_date')
-
-
 class CategorySerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:category-detail')
+
     class Meta:
         model = Category
-        fields = ('id', 'name', 'description')
+        fields = ('url', 'id', 'name', 'description')
 
 
-class HallSerializer(serializers.ModelSerializer):
+class InlineCategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Hall
+        model = Category
         fields = ('id', 'name')
 
 
+class MovieSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:movie-detail')
+    genre = InlineCategorySerializer(many=True)
+
+    class Meta:
+        model = Movie
+        fields = ('url', 'id', 'name', 'genre','description', 'poster', 'release_date', 'finish_date')
+
+
+class HallSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:hall-detail')
+
+    class Meta:
+        model = Hall
+        fields = ('url', 'id', 'name')
+
+
 class SeatSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:seat-detail')
+    hall_url = serializers.HyperlinkedRelatedField(view_name='api_v1:hall-detail', source='hall', read_only=True)
+
     class Meta:
         model = Seat
-        fields = ('id', 'hall', 'row', 'place')
+        fields = ('url', 'id', 'hall', 'hall_url', 'row', 'place')
+
+
+class InlineSeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seat
+        fields = ('hall', 'row', 'place')
 
 
 class ShowSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:show-detail')
+    film_url = serializers.HyperlinkedRelatedField(view_name='api_v1:movie-detail', source='film', read_only=True)
+    hall_url = serializers.HyperlinkedRelatedField(view_name='api_v1:hall-detail', source='hall', read_only=True)
+
     class Meta:
         model = Show
-        fields = ('id', 'film', 'hall', 'start', 'finish', 'price')
+        fields = ('url', 'id', 'film', 'film_url', 'hall', 'hall_url', 'start', 'finish', 'price')
+
+
+class InlineShowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Show
+        fields = ('film', 'price')
+
+
+class DiscountSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:discount-detail')
+
+    class Meta:
+        model = Discount
+        fields = ('url', 'id', 'name', 'discount', 'discount_start', 'discount_finish')
+
+
+class InlineDiscountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discount
+        fields = ('name', 'discount')
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:ticket-detail')
+    # discount = InlineDiscountSerializer(many=True)
+    # show = InlineShowSerializer(many=True)
+    # seat = InlineSeatSerializer(many=True)
+
+    class Meta:
+        model = Ticket
+        fields = ('url', 'show', 'seat', 'discount', 'return_ticket')
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:reservation-detail')
+    # seat = InlineSeatSerializer(many=True)
+    # show = InlineShowSerializer(many=True)
+
+    class Meta:
+        model = Reservation
+        fields = ('url', 'id', 'show', 'seats', 'status', 'created_at', 'changed_at')
