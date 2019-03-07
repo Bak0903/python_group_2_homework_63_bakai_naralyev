8263081c82e9788aa377aa67e0da294c104bb14c
bak0903 <bak0903@mail.ru>
2019-03-07 20:40:51 +0600

    Добавил контейнер AllMovies, где запрашивается список всех фильмов.

---
+++ frontend/src/containers/AllMovies/AllMovies.js
@@ -0,0 +1,69 @@
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import React, {Component} from 'react';
+import './AllMovies.css';
+import GetList from '../../consructors/GetList/GetList';
+import axios from 'axios';
+
+
+class AllMovies extends Component {
+
+    state = {
+        allMovies: [],
+    };
+
+    getAll = () => {
+        axios.get('movies').then(response => {
+            const requests = response.data.map(movie => {
+                return {
+                    id: movie.id,
+                    genre: movie.genre,
+                    name: movie.name
+                };
+            });
+            return Promise.all(requests);})
+            .then(allMovies => {this.setState({allMovies});})
+            .catch(error => {console.log(error);});
+    };
+
+    purchaseContinueHandler = (pathWord) => {
+        this.props.history.push({pathname: '/' + pathWord});
+    };
+
+
+    // getInfo = (code) => {
+    //     axios.get('alpha/' + code).then(response => {
+    //         const borders = response.data.borders.map(element => {
+    //             return axios.get('alpha/' + element).then(response => {
+    //                 return response.data;
+    //             });
+    //         });
+    //         return Promise.all(borders)
+    //             .then(neighbour => this.setState({
+    //                 selectedCountry: response.data,
+    //                 borders: [...neighbour]
+    //             }));
+    //     })
+    // };
+
+    componentDidMount() {
+        this.getAll();
+    }
+
+    render() {
+        return (
+            <div className={'AllMovies'}>
+                <GetList
+                    list={this.state.allMovies}
+                    click={this.purchaseContinueHandler}
+                />
+                {/*<Country*/}
+                    {/*info={this.state.selectedCountry}*/}
+                    {/*borders={this.state.borders}*/}
+                    {/*click={this.getInfo}*/}
+                {/*/>*/}
+            </div>
+        );
+    }
+}
+
+export default AllMovies;
