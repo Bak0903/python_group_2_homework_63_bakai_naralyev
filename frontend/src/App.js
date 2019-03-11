8cc9a17207b5f67eaa0478b8339d6af410594966
bak0903 <bak0903@mail.ru>
2019-03-11 16:38:19 +0600

    Добавил BrowserRouter в App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import AllMovies from './containers/AllMovies/AllMovies';
+import SelectedMovie from './containers/SelectedMovie/SelectedMovie';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={'App'}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/SelectedMovie/:id" component={SelectedMovie}/>
+                        <Route path="/" exact component={AllMovies}/>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
+
